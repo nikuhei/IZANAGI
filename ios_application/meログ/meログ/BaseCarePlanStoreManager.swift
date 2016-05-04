cee134026f7e304cd4e8fa39f6e0f07de505374a
nikuhei <i.24zawa@icloud.com>
2016-05-04 10:04:40 +0900

    CareKit実装中.

---
+++ ios_application/meログ/meログ/BaseCarePlanStoreManager.swift
@@ -0,0 +1,60 @@
+//
+//  BaseCarePlanStoreManager.swift
+//  meログ
+//
+//  Created by 西澤 郁平 on 2016/05/04.
+//  Copyright © 2016年 ikunyan.org. All rights reserved.
+//
+
+import Foundation
+import CareKit
+
+class BaseCarePlanStoreManager: NSObject {
+    
+    let CarePlanStoreURL = "MeLogCarePlanStore"
+    
+    let store: OCKCarePlanStore
+    
+    static var sharedCarePlanStoreManager = BaseCarePlanStoreManager()
+    weak var delegate: BaseCarePlanStoreManagerDelegate?
+    
+    override init() {
+        let fileManager = NSFileManager.defaultManager()
+        guard let documentDirectory = fileManager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).last else {
+            fatalError("*** Error: Unable to get the document directory! ***")
+        }
+        let storeURL = documentDirectory.URLByAppendingPathComponent(CarePlanStoreURL)
+        
+        if !fileManager.fileExistsAtPath(storeURL.path!) {
+            try! fileManager.createDirectoryAtURL(storeURL, withIntermediateDirectories: true, attributes: nil)
+        }
+        store = OCKCarePlanStore(persistenceDirectoryURL: storeURL)
+        
+        super.init()
+        
+        store.delegate = self
+    }
+    
+    func getCarePlanStore() -> OCKCarePlanStore {
+        return store
+    }
+    
+    func updateView() {
+        let dictionary = NSDictionary()
+        
+        guard let storeManager:BaseCarePlanStoreManager = self else {
+            return
+        }
+        storeManager.delegate?.baseCarePlanStoreManager(storeManager, updateRecords: dictionary)
+    }
+}
+
+extension BaseCarePlanStoreManager: OCKCarePlanStoreDelegate {
+    func carePlanStore(store: OCKCarePlanStore, didReceiveUpdateOfEvent event: OCKCarePlanEvent) {
+        updateView()
+    }
+}
+
+protocol BaseCarePlanStoreManagerDelegate: class {
+    func baseCarePlanStoreManager(manager: BaseCarePlanStoreManager, updateRecords records:NSDictionary)
+}
---
+++ ios_application/meログ/meログ/MCareCardViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MCareCardViewController.swift
+//  meログ
+//
+//  Created by 西澤 郁平 on 2016/05/03.
+//  Copyright © 2016年 ikunyan.org. All rights reserved.
+//
+
+import UIKit
+import CareKit
+
+class MCareCardViewController: OCKCareCardViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
